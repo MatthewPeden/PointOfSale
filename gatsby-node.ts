e5f8d1a4bebb589b1db9f8aae9fc2ca4c4a1f262
ccwky11 <ccwky11@gmail.com>
2023-04-05 14:01:31 -0500

    nuked the project for nextjs transition

--- gatsby-node.ts
+++
@@ -1,8 +0,0 @@
-exports.onCreatePage = async ({ page, actions }) => {
-    const { createPage } = actions
-    
-    if (page.path.match(/^\/account/)) {
-      page.matchPath = "/account/*"
-      createPage(page)
-    }
-  }
