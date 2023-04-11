37ddd220a731f1b38680a2513ac67a629014c26c
ccwky11 <ccwky11@gmail.com>
2023-04-11 12:34:15 -0500

    adjusted index page to require login before displaying any information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,34 +3,35 @@ import Profile from '@/components/Profile';
 
 // Import the Link component from Next.js
 import Link from 'next/link';
+//Import useUser from auth0 for if statements and such
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 
 // Define a custom component for the page
 export default function Home() {
-
+  const { user, error, isLoading } = useUser();
 // If the user is logged in, show a welcome message and a logout button
-
+if (user){
     return (
       <div>
-        <h1>Welcome!</h1>
-        <Profile></Profile>
-<a href="/api/auth/login">Login</a>
+        <h1>Welcome {user.name}!</h1>
         <p>You are logged in with Auth0.</p>
         <a href="/api/auth/logout">Log out</a>
         <ul>
           <li><Link href="/drawTest">Draw Test</Link></li>
-          <li><Link href="/payments_report">Payments Report</Link></li>
-          <li><Link href="/reports_test">Reports Test</Link></li>
-          <li><Link href="/inventory_items_report">Inventory Items Report</Link></li>
-          <li><Link href="/orders_report">Orders Report</Link></li>
-          <li><Link href="/protected">Protected</Link></li>
+          <li><Link href="/payments_report" style={{color: 'red'}}>Payments Report (dead link)</Link></li>
+          <li><Link href="/reports_test"style={{color: 'red'}}>Reports Test (dead link)</Link></li>
+          <li><Link href="/inventory_items_report"style={{color: 'red'}}>Inventory Items Report (dead link)</Link></li>
+          <li><Link href="/orders_report"style={{color: 'red'}}>Orders Report (dead link)</Link></li>
+          <li><Link href="/categories">Categories</Link></li>
         </ul>
       </div>
     );
-
+    }
   // If the user is not logged in, show a login button
   return (
     <div>
-      <h1>Next.js Auth0 Example</h1>
+      <h1>Zamaco POS</h1>
       <p>Please log in to continue.</p>
       <a href="/api/auth/login">Log in</a>
     </div>
