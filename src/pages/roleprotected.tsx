cdd6cb6ceb4669159961770b3169beeea1c62ad4
ccwky11 <ccwky11@gmail.com>
2023-04-13 16:30:43 -0500

    modularized Role-Based Authentication

--- src/pages/roleprotected.tsx
+++ src/pages/roleprotected.tsx
@@ -11,9 +11,23 @@ export default function Admin({ error }: Props) {
 
 export async function getServerSideProps({ req, res }: NextPageContext) {
   const session = await getSession(req, res);
-  if (!session?.user['https://zamacopos.com/roles'].includes('Manager')) {
+  const user = session?.user;
+
+  // If there's no user logged in, redirect to /login
+  if (!user) {
+    return { redirect: { destination: '/api/auth/login', permanent: false } };
+  }
+
+  // If there's a user logged in but not verified, redirect to /verify
+  if (!user['https://zamacopos.com/roles'].includes('Manager')) {
+    return { redirect: { destination: '/not-authorized', permanent: false } };
+  }
+
+  // If there's a user logged in and verified but not a manager, return an error
+  if (!user['https://zamacopos.com/roles'].includes('Manager')) {
     return { props: { error: 'Forbidden' } };
   }
 
+  // If there's a user logged in, verified and a manager, continue to /admin
   return { props: {} };
 }
