301abef655991d4675978780f3d9e0d9c3904361
ccwky11 <ccwky11@gmail.com>
2023-04-22 13:41:02 -0500

    deleted unneeded files

--- src/pages/protected.tsx
+++
@@ -1,8 +0,0 @@
-// pages/protected-page.js
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-
-export default function ProtectedPage() {
-  return <div>Protected content</div>;
-}
-
-export const getServerSideProps = withPageAuthRequired();
