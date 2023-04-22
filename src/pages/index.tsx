e9826da479e1548c7f034978952c82472a10422e
ccwky11 <ccwky11@gmail.com>
2023-04-22 13:26:04 -0500

    adjusted styling further

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,112 @@
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
 
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-  },
-  heading: {
-    fontSize: '2rem',
-    fontWeight: 'bold',
-    marginBottom: '1rem',
-  },
-  text: {
-    fontSize: '1.5rem',
-    marginBottom: '1rem',
-  },
-  button: {
-    fontSize: '1.5rem',
-    padding: '0.5rem 1rem',
-    backgroundColor: 'blue',
-    color: 'white',
-    borderRadius: '0.5rem',
-    cursor: 'pointer',
-  },
-};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  min-height: calc(100vh - 60px);
+  background-color: #ede6f5;
+  padding: 20px;
+  padding-top: 40px;
+`;
+
+const Title = styled.h1`
+  color: #8447c9;
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const Text = styled.p`
+  color: #8447c9;
+  font-size: 18px;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 0.5rem 1rem;
+  background-color: #5f4b8b;
+  color: white;
+  border-radius: 30px;
+  cursor: pointer;
+  border: none;
+  &:hover {
+    background-color: #7d6ba0;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  margin-bottom: 5px;
+  color: #5f4b8b;
+  font-size: 16px;
+  text-decoration: none;
+  &:hover {
+    color: #7d6ba0;
+  }
+`;
+
+const CenteredLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 40px;
+`;
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
 
   if (user) {
     return (
-      <div style={styles.container}>
-        <h1 style={styles.heading}>Welcome {user.name}!</h1>
-        <p style={styles.text}>You are logged in with Auth0.</p>
-        <a href="/api/auth/logout">Log out</a>
-        <ul>
-          <li>
-            <Link href="/drawTest">Draw Test</Link>
-          </li>
-          <li>
-            <Link href="/payments_report">Payments Report</Link>
-          </li>
-          <li>
-            <Link href="/inventory_items_report">Inventory Items Report</Link>
-          </li>
-          <li>
-            <Link href="/orders_report">Orders Report</Link>
-          </li>
-          <li>
-            <Link href="/categories">Categories</Link>
-          </li>
-          <li>
-            <Link href="/products">Products</Link>
-          </li>
-          <li>
-            <Link href="/inventory_items">Inventory Items</Link>
-          </li>
-          <li>
-            <Link href="/product_inventory_items">Product Items</Link>
-          </li>
-          <p></p>
-          <li>
-            <Link href="/roleprotected">NEW ROLE TESTING</Link>
-          </li>
-        </ul>
-      </div>
+      <Container>
+        <Layout>
+          <Title>Welcome {user.name}!</Title>
+          <Text>You are logged in with Auth0.</Text>
+          <CenteredLinks>
+            <ul>
+              <li>
+                <StyledLink href="/drawTest">Draw Test</StyledLink>
+              </li>
+             <li>
+              <StyledLink href="/payments_report">Payments Report</StyledLink>
+            </li>
+            <li>
+              <StyledLink href="/inventory_items_report">Inventory Items Report</StyledLink>
+            </li>
+            <li>
+              <StyledLink href="/orders_report">Orders Report</StyledLink>
+            </li>
+            <li>
+              <StyledLink href="/categories">Categories</StyledLink>
+            </li>
+            <li>
+              <StyledLink href="/products">Products</StyledLink>
+            </li>
+            <li>
+              <StyledLink href="/inventory_items">Inventory Items</StyledLink>
+            </li>
+            <li>
+              <StyledLink href="/product_inventory_items">Product Items</StyledLink>
+            </li>
+            <p></p>
+          </ul>
+          </CenteredLinks>
+        </Layout>
+      </Container>
     );
   } else {
     return (
-      <div style={styles.container}>
-        <h1 style={styles.heading}>Zamaco POS</h1>
-        <p style={styles.text}>Please log in to continue.</p>
-        <button style={styles.button} onClick={() => (window.location.href = '/api/auth/login')}>
-          Log in
-        </button>
-      </div>
+      <Container>
+        <Title>Zamaco POS</Title>
+        <Text>Please log in to continue.</Text>
+        <Button onClick={() => (window.location.href = '/api/auth/login')}>Log in</Button>
+      </Container>
     );
   }
 }
